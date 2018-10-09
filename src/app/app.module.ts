6341ff405551ee838db7826b778b75b8f6bcbc26
Diana Blanco <dbpautt@gmail.com>
2018-10-09 14:51:45 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+import { InitAuthGuard } from './guards/init-auth.guard';
+import { RequireAnonGuard } from './guards/require-anon.guard';
+import { RequireUserGuard } from './guards/require-user.guard';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent, canActivate: [InitAuthGuard] },
+  { path: 'login',  component: LoginPageComponent, canActivate: [RequireAnonGuard] },
+  { path: 'profile',  component: ProfilePageComponent, canActivate: [RequireUserGuard] },
+  { path: '**',  component: NotFoundPageComponent, canActivate: [InitAuthGuard] }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    NotFoundPageComponent,
+    ProfilePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-profile-page',
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.scss']
+})
+export class ProfilePageComponent implements OnInit {
+
+  user: any;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.user = this.authService.getUser();
+
+  }
+
+}
